68798c6b00ca469c2cb81d380ea9aff4c43e652b
ChequedMetal <kostiaandres@gmail.com>
2025-07-02 19:37:54 -0400

    funciona android

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyACcdyM0M-SUUeD4KeeVO3Jqn-esuOXzM4",
+    authDomain: "condapp1-f384f.firebaseapp.com",
+    projectId: "condapp1-f384f",
+    storageBucket: "condapp1-f384f.firebasestorage.app",
+    messagingSenderId: "749569492268",
+    appId: "1:749569492268:web:f992efa3c7206d76cdd889"
+  }
 };
